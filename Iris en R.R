85bd84b27a92a62c7d060e414dc0fe159c8177ef
JuanCarlosRondeau <juancarrc@gmail.com>
2020-10-08 11:32:20 +0200

    Añadiendo un archivo R

---
+++ Iris en R.R
@@ -0,0 +1,28 @@
+# 21816176 Juan Carlos Rondeau Cadarso
+# Titulo: Proyecto Suicidios
+
+#Cargamos el dataset y lo guardamos en la variable
+#data <- read.csv("Proyecto-Open-Data-2/master.csv")
+
+library(datasets) #importamos el dataset
+?iris
+df <- iris 
+head(df)
+df
+
+#Histograma
+hist(df$Petal.Width , main = "European University - Histogram of Petal Width" , xlab = "Petal Width (in cm)")
+
+#Clear environment
+rm(list = ls())
+
+#Clear packages
+detach("packeage:datasets" , unload = TRUE)
+
+#Clear plots
+graphics.off()
+
+#Clear console
+cat("\014")   # ctrl + L
+
+
