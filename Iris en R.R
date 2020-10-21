41b30b932e51f8bef26c994ac3e80c827793a1ad
JuanCarlosRondeau <juancarrc@gmail.com>
2020-10-21 10:16:30 +0200

    he cambiado algo

--- Iris en R.R
+++ Iris en R.R
@@ -9,7 +9,7 @@ library(datasets) #importamos el dataset
 df <- iris 
 head(df)
 df
-
+holaquetal <- 98
 #Histograma
 hist(df$Petal.Width , main = "European University - Histogram of Petal Width" , xlab = "Petal Width (in cm)")
 
@@ -21,6 +21,6 @@ detach("packeage:datasets" , unload = TRUE)
 
 #Clear plots
 graphics.off()
-
+jkdhgfrk
 #Clear console
 cat("\014")   # ctrl + L
